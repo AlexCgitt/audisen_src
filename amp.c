9a57bdc06f6276888e9aaceee81f7830e49ae0bc
AlexCgitt <alexmicrosoft03@gmail.com>
2024-03-04 11:48:17 +0100

    Initial commit

---
+++ amp.c
@@ -0,0 +1,23 @@
+/**
+ * @authors CAVARO Alexandre RIOU Margot 2 Caen
+ */
+
+#include "define.h"
+#include "amp.h"
+
+
+FILE* initAMP(char* filename){
+    FILE * pf = NULL;
+   
+    return pf;
+}
+
+
+
+void readAMP(FILE* pf, char * song_filename){
+
+}
+
+void closeAMP(FILE* pf){
+    
+}
